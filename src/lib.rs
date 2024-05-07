8c095968730b838a1105e3898235246a797275a1
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-05-06 23:58:55 -0400

    [src/lib.rs] Start to support conditionals on all relevant fields using JSON-schema style syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,102 @@
 extern crate serde;
 
 use serde_derive::{Deserialize, Serialize};
+use std::fmt;
+
+impl<'de, T: serde::Deserialize<'de>> serde::Deserialize<'de> for ValXorIfThenElse<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+    {
+        struct StringOrStruct<T>(std::marker::PhantomData<fn() -> T>);
+
+        impl<'de, T: serde::Deserialize<'de>> serde::de::Visitor<'de> for StringOrStruct<T> {
+            type Value = ValXorIfThenElse<T>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("string or structure")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<ValXorIfThenElse<T>, E>
+                where
+                    E: serde::de::Error,
+            {
+                Ok(ValXorIfThenElse::Val(value.to_owned()))
+            }
+
+            fn visit_map<A>(self, map: A) -> Result<ValXorIfThenElse<T>, A::Error>
+                where
+                    A: serde::de::MapAccess<'de>,
+            {
+                Ok(ValXorIfThenElse::IfThenElse(serde::Deserialize::deserialize(
+                    serde::de::value::MapAccessDeserializer::new(map),
+                )?))
+            }
+        }
+
+        deserializer.deserialize_any(StringOrStruct(std::marker::PhantomData))
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(deserialize_with = "des_val_xor_if_then_else")]
+pub enum ValXorIfThenElse<T> {
+    Val(T),
+    IfThenElse {
+        #[serde(rename = "if")]
+        if_field: String,
+        then: T,
+        #[serde(rename = "else")]
+        else_field: Option<T>,
+    },
+}
+
+union StringOrT<T> {
+    string: std::mem::ManuallyDrop<String>,
+    t: std::mem::ManuallyDrop<T>
+}
+
+impl Default for ValXorIfThenElse<String> {
+    fn default() -> Self {
+        ValXorIfThenElse::<String>::IfThenElse {
+            if_field: String::from("true"), then: String::from("true"), else_field: None
+        }
+    }
+}
+
+/*impl <T>Into<ValXorIfThenElse<std::string::String>> for ValXorIfThenElse<T> {
+    fn into(self) -> ValXorIfThenElse<String> {
+        ValXorIfThenElse::Val(self)
+    }
+}*/
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseValXorIfThenElseError;
+
+impl std::str::FromStr for ValXorIfThenElse<String> {
+    type Err = ParseValXorIfThenElseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(ValXorIfThenElse::<String>::Val(s.into()))
+    }
+}
+
+impl From<&str> for ValXorIfThenElse<String> {
+    fn from(s: &str) -> Self {
+        ValXorIfThenElse::<String>::Val(s.into())
+    }
+}
+impl <T>From<T> for ValXorIfThenElse<T> {
+    fn from(t: T) -> Self {
+        ValXorIfThenElse::<T>::Val(t)
+    }
+}
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Root {
-    pub name: String,
-    pub version: String,
+    pub name: ValXorIfThenElse<String>,
+    pub version: ValXorIfThenElse<String>,
     pub license: String,
     pub homepage: String,
     pub repo: String,
@@ -20,11 +111,11 @@ pub struct Root {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct StateValues {
-    pub kind: Option<String>,
-    pub install: Option<State>,
-    pub remove: Option<State>,
-    pub start: Option<State>,
-    pub stop: Option<State>,
+    pub kind: Option<ValXorIfThenElse<String>>,
+    pub install: Option<ValXorIfThenElse<State>>,
+    pub remove: Option<ValXorIfThenElse<State>>,
+    pub start: Option<ValXorIfThenElse<State>>,
+    pub stop: Option<ValXorIfThenElse<State>>,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -67,25 +158,25 @@ pub struct ServerConfiguration {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct ProtocolConfiguration {
     /// E.g., "localhost" | "127.0.0.1" | "::1" | "my_name.verman.io"
-    pub name: String,
+    pub name: ValXorIfThenElse<String>,
 
     /// E.g., "https" | "http"
-    pub protocol: String,
+    pub protocol: ValXorIfThenElse<String>,
 
     /// E.g., "LetsEncrypt"
-    pub certificate_vendor: Option<String>,
+    pub certificate_vendor: Option<ValXorIfThenElse<String>>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Component {
-    pub src: Option<String>,
-    pub kind: String,
-    pub version: Option<String>,
-    pub uri: String,
+    pub src: Option<ValXorIfThenElse<String>>,
+    pub kind: ValXorIfThenElse<String>,
+    pub version: Option<ValXorIfThenElse<String>>,
+    pub uri: ValXorIfThenElse<String>,
     /* `vendor` example: {"nginx": {"windows": "./win_nginx.site_avail.conf",
     "_": "./nginx.site_avail.conf"}} */
-    pub vendor: Option<indexmap::IndexMap<String, indexmap::IndexMap<Os, KindAndLocation>>>,
-    pub mounts: Option<indexmap::IndexMap<String, KindAndLocation>>,
+    pub vendor: Option<indexmap::IndexMap<String, indexmap::IndexMap<Os, KindAndUri>>>,
+    pub mounts: Option<indexmap::IndexMap<String, KindAndUri>>,
 }
 
 /// OSs from https://github.com/rust-lang/rust/blob/1.77.0/library/std/src/env.rs#L947-L961
@@ -114,76 +205,81 @@ impl Default for Os {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct VendorVersion {
-    pub vendor: String,
-    pub version: Option<String>,
+    pub vendor: ValXorIfThenElse<String>,
+    pub version: Option<ValXorIfThenElse<String>>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct KindAndLocation {
-    pub kind: String,
-    pub location: String,
+pub struct KindAndUri {
+    pub kind: ValXorIfThenElse<String>,
+    pub uri: ValXorIfThenElse<String>,
 }
+
+/* pub fn eval_field(s: Box<dyn Into<String>>) -> String {
+    s.into()
+} */
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     const VERMAN_JSON: &'static str = include_str!("verman.json");
     const VERMAN_TOML: &'static str = include_str!("verman.toml");
 
     #[test]
     fn it_serdes() {
         let config = Root {
-            name: String::from(env!("CARGO_PKG_NAME")),
-            version: String::from(env!("CARGO_PKG_VERSION")),
+            name: String::from(env!("CARGO_PKG_NAME")).into(),
+            version: String::from(env!("CARGO_PKG_VERSION")).into(),
             license: String::from("(Apache-2.0 OR MIT)"),
             homepage: String::from("https://verman.io"),
             repo: String::from("https://github.com/verman-io"),
             authors: vec![String::from(env!("CARGO_PKG_AUTHORS"))],
 
-            stack_state:  {
-                    let mut state = indexmap::IndexMap::<String, StateValues>::new();
-                    state.insert(
-                        String::from("database"),
-                        StateValues {
-                            kind: Some(String::from("sql")),
-                            install: Some(State::Always),
-                            remove: None,
-                            start: Some(State::Always),
-                            stop: None,
-                        },
-                    );
-                    state.insert(
-                        String::from("application_server"),
-                        StateValues {
-                            kind: None,
-                            install: Some(State::Always),
-                            remove: None,
-                            start: Some(State::Always),
-                            stop: None,
-                        },
-                    );
-                    state.insert(
-                        String::from("routing"),
-                        StateValues {
-                            kind: None,
-                            install: Some(State::Always),
-                            remove: None,
-                            start: Some(State::Always),
-                            stop: None,
-                        },
-                    );
-                    state
+            stack_state: {
+                let mut state = indexmap::IndexMap::<String, StateValues>::new();
+                state.insert(
+                    String::from("database"),
+                    StateValues {
+                        kind: Some(String::from("sql").into()),
+                        install: Some(State::Always.into()),
+                        remove: None,
+                        start: Some(State::Always.into()),
+                        stop: None,
+                    },
+                );
+                state.insert(
+                    String::from("application_server"),
+                    StateValues {
+                        kind: None,
+                        install: Some(State::Always.into()),
+                        remove: None,
+                        start: Some(State::Always.into()),
+                        stop: None,
+                    },
+                );
+                state.insert(
+                    String::from("routing"),
+                    StateValues {
+                        kind: None,
+                        install: Some(State::Always.into()),
+                        remove: None,
+                        start: Some(State::Always.into()),
+                        stop: None,
+                    },
+                );
+                state
             },
 
             stack: {
-              let mut stack = indexmap::IndexMap::<String, Vec<ServerConfiguration>>::new();
+                let mut stack = indexmap::IndexMap::<String, Vec<ServerConfiguration>>::new();
                 stack.insert(
-                   String::from("database"),
-                   vec![ServerConfiguration {
-                       kind: String::from("sql"),
-                       versions: None,
-                       server_priority: None,
-                   }]
+                    String::from("database"),
+                    vec![ServerConfiguration {
+                        kind: String::from("sql"),
+                        versions: None,
+                        server_priority: None,
+                    }],
                 );
                 stack.insert(
                     String::from("application_server"),
@@ -206,69 +302,69 @@ mod tests {
                             versions: None,
                             server_priority: None,
                         },
-                    ]
+                    ],
                 );
                 stack
             },
-            stack_routing: vec![ProtocolConfiguration{
-                name: String::from("my_name.verman.io"),
-                protocol: String::from("https"),
-                certificate_vendor: Some(String::from("LetsEncrypt")),
+            stack_routing: vec![ProtocolConfiguration {
+                name: String::from("my_name.verman.io").into(),
+                protocol: String::from("https").into(),
+                certificate_vendor: Some(String::from("LetsEncrypt").into()),
             }],
             component: vec![
-                Component{
-                    src: Some(String::from("./python_api_folder/")),
-                    kind: String::from("python"),
-                    version: Some(String::from(">3.8")),
-                    uri: String::from("http://localhost:${env.PYTHON_API_PORT}"),
+                Component {
+                    src: Some(String::from("./python_api_folder/").into()),
+                    kind: String::from("python").into(),
+                    version: Some(String::from(">3.8").into()),
+                    uri: String::from("http://localhost:${env.PYTHON_API_PORT}").into(),
                     vendor: None,
                     mounts: None,
                 },
-                Component{
-                    src: Some(String::from("./ruby_api_folder/")),
-                    kind: String::from("ruby"),
-                    version: Some(String::from(">3.1.2, <3.2")),
-                    uri: String::from("${if(WIN32) { \"\\\\.\\pipe\\PipeName\" } else { \"unix:///var/run/my-socket.sock\" }}"),
+                Component {
+                    src: Some(String::from("./ruby_api_folder/").into()),
+                    kind: String::from("ruby").into(),
+                    version: Some(String::from(">3.1.2, <3.2").into()),
+                    uri: String::from("${if(WIN32) { \"\\\\.\\pipe\\PipeName\" } else { \"unix:///var/run/my-socket.sock\" }}").into(),
                     vendor: None,
                     mounts: None,
                 },
-                Component{
+                Component {
                     src: None,
-                    kind: String::from("routing"),
+                    kind: String::from("routing").into(),
                     version: None,
-                    uri: String::from("my_app.verman.io"),
+                    uri: String::from("my_app.verman.io").into(),
                     vendor: {
-                        let mut vendor = indexmap::IndexMap::<String, indexmap::IndexMap::<Os, KindAndLocation>>::new();
+                        let mut vendor = indexmap::IndexMap::<String, indexmap::IndexMap::<Os, KindAndUri>>::new();
                         vendor.insert(String::from("nginx"), {
-                            let mut os_to_kind_and_location = indexmap::IndexMap::<Os, KindAndLocation>::new();
-                            os_to_kind_and_location.insert(Os::Windows, KindAndLocation { kind: String::from("server_block"), location: String::from("./win_nginx.site_avail.conf") });
-                            os_to_kind_and_location.insert(Os::Linux, KindAndLocation { kind: String::from("server_block"), location: String::from("./nginx.site_avail.conf") });
+                            let mut os_to_kind_and_location = indexmap::IndexMap::<Os, KindAndUri>::new();
+                            os_to_kind_and_location.insert(Os::Windows, KindAndUri { kind: String::from("server_block").into(), uri: String::from("./win_nginx.site_avail.conf").into() });
+                            os_to_kind_and_location.insert(Os::Linux, KindAndUri { kind: String::from("server_block").into(), uri: String::from("./nginx.site_avail.conf").into() });
                             os_to_kind_and_location
                         });
                         Some(vendor)
                     },
                     mounts: {
-                        let mut mounts = indexmap::IndexMap::<String, KindAndLocation>::new();
+                        let mut mounts = indexmap::IndexMap::<String, KindAndUri>::new();
                         mounts.insert(
                             String::from("/api/py"),
-                            KindAndLocation {
-                                kind: String::from("python"),
-                                location: String::from("${stack.components[.kind==\"python\"].uri}")
-                            }
+                            KindAndUri {
+                                kind: String::from("python").into(),
+                                uri: String::from("${stack.components[.kind==\"python\"].uri}").into(),
+                            },
                         );
                         mounts.insert(
                             String::from("/api/ruby"),
-                            KindAndLocation {
-                                kind: String::from("ruby"),
-                                location: String::from("${stack.components[.kind==\"ruby\"].uri}")
-                            }
+                            KindAndUri {
+                                kind: String::from("ruby").into(),
+                                uri: String::from("${stack.components[.kind==\"ruby\"].uri}").into(),
+                            },
                         );
                         mounts.insert(
                             String::from("/"),
-                            KindAndLocation {
-                                kind: String::from("static"),
-                                location: String::from("${env.WWWROOT}")
-                            }
+                            KindAndUri {
+                                kind: String::from("static").into(),
+                                uri: String::from("file://${env.WWWROOT}").into(),
+                            },
                         );
                         Some(mounts)
                     },
@@ -277,8 +373,9 @@ mod tests {
         };
         let root_from_json: Root = serde_json::from_str(&VERMAN_JSON).unwrap();
         let root_from_toml: Root = toml::from_str(&VERMAN_TOML).unwrap();
-        std::fs::write("./src/verman.toml", toml::to_string(&config).unwrap())
-            .expect("Could not write to file!");
+        /* std::fs::write("./src/verman.toml", toml::to_string(&config).unwrap())
+        .expect("Could not write to file!"); */
+        assert_eq!(root_from_toml, root_from_json);
         assert_eq!(
             serde_json::to_string(&config).unwrap(),
             serde_json::to_string(&root_from_json).unwrap()
